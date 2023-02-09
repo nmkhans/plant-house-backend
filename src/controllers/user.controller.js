b7e0e44b2ae716674faeb53c92e202c3ca9819fb
nmkhans <nurmoin05@gmail.com>
2023-02-09 12:08:36 +0600

    user registration system added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,42 @@
 const User = require("../models/user.model")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports.registerUser = async (req, res, next) => {
     try {
-        console.log(req.body)
-    } catch (error) {
+        const data = req.body;
+        console.log(data)
+        const userExist = await User.findOne(
+            { email: data.email },
+            { _id: 0, email: 1 }
+        );
 
+        if (!userExist) {
+            const hashedPassword = await bcrypt.hash(data.password, 10);
+            const userData = {
+                ...data,
+                password: hashedPassword
+            }
+            await User.create(userData);
+            const user = await User.findOne(
+                { email: data.email },
+                { password: 0 }
+            );
+
+            res.status(200).json({
+                success: true,
+                message: "User registration successfull.",
+                data: user
+            })
+
+        } else {
+            res.status(500).json({
+                success: false,
+                message: "User already exist!"
+            })
+        }
+
+    } catch (error) {
+        next(error)
     }
 }
