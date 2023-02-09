2e12328a7a90e9ada0f10363f4e92661d61b2e7e
nmkhans <nurmoin05@gmail.com>
2023-02-09 11:34:39 +0600

    database configured and connected to atlas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 require("dotenv").config()
 const cors = require("cors")
+const mongoose = require("mongoose")
 const defaultRoute = require("./src/routes/default.route")
 
 //? app configuration
@@ -9,6 +10,17 @@ app.use(cors())
 app.use(express.json())
 
 //? database configuration
+const uri = process.env.DB_URI
+const databaseConfig = {
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS
+}
+
+//? database connection
+mongoose.set("strictQuery", true)
+mongoose.connect(uri, databaseConfig)
+    .then(() => console.log("database connected"))
+    .catch((error) => console.log(error))
 
 //? handle routes
 app.use("/api/v1", defaultRoute)
