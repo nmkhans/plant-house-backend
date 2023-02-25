1f3d445934b7de65c5ee2388a9649c124fda488c
nmkhans <nurmoin05@gmail.com>
2023-02-25 14:11:42 +0600

    category delete feature added

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -19,11 +19,27 @@ module.exports.createCategories = async (req, res, next) => {
 module.exports.getCategories = async (req, res, next) => {
     try {
         const result = await Category.find()
-        
+
         res.status(200).json({
             success: true,
             message: "All categories.",
-            data: result 
+            data: result
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports.deleteCategory = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const result = await Category.deleteOne({ _id: id })
+
+        res.status(200).json({
+            success: true,
+            message: "Category deleted!",
+            data: result
         })
 
     } catch (error) {
--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -3,25 +3,38 @@ const router = express.Router()
 const categoryController = require("../controllers/category.controller")
 
 router
-    .route("/")
-    /**
-     * @api (get) /categories 
-     * @apiDescription get all categories
-     * @apiPermission users
-     * 
-    */
-    .get(categoryController.getCategories)
-    /**
-      * @api (post) /categories 
-      * @apiDescription create new categories
-      * @apiPermission admin
-      * 
-      * @apiHeader Authorization admins's access token
-      * 
-      * @apiError (unauthorized 401) Unauthorized access. only authenticated admin can access 
-      * @apiError (forbidden 403) Forbidden access. only admin can access 
-      * 
-    */
-    .post(categoryController.createCategories)
+  .route("/")
+  /**
+   * @api (get) /categories 
+   * @apiDescription get all categories
+   * @apiPermission users
+   * 
+  */
+  .get(categoryController.getCategories)
+  /**
+    * @api (post) /categories 
+    * @apiDescription create new categories
+    * @apiPermission admin
+    * 
+    * @apiHeader Authorization admins's access token
+    * 
+    * @apiError (unauthorized 401) Unauthorized access. only authenticated admin can access 
+    * @apiError (forbidden 403) Forbidden access. only admin can access 
+    * 
+  */
+  .post(categoryController.createCategories)
+  /**
+    * @api (delete) /categories/:id
+    * @apiDescription delete categories
+    * @apiPermission admin
+    * 
+    * @apiHeader Authorization admins's access token
+    * 
+    * @apiError (unauthorized 401) Unauthorized access. only authenticated admin can access 
+    * @apiError (forbidden 403) Forbidden access. only admin can access 
+    * 
+  */
+
+router.delete("/delete/:id", categoryController.deleteCategory)
 
 module.exports = router
